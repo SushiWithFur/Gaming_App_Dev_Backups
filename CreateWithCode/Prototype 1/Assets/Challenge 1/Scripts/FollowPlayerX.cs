7cb25b41b368137050e3581e9da00dd8412766a7
Cooper Christensen <cooper.c757@stu.nebo.edu>
2019-09-26 09:55:32 -0600

    Finished Challenge

--- CreateWithCode/Prototype 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ CreateWithCode/Prototype 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset = new Vector3(32, 4, -3);
+    private Vector3 offset = new Vector3(17, 2, (float)-0.5);
     // Update is called once per frame
     void Update()
     {
