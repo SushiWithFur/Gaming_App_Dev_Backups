3f62b3a7896efe3647faa1527625638946273615
Cooper <cooper.c757@stu.nebo.edu>
2019-11-12 14:13:09 -0700

    Finished 3.2 or 3.3 or 3.4 for prototype 3 idk which one it was

--- CreateWithCode/Prototype 3/Assets/Scripts/MoveLeft.cs
+++ CreateWithCode/Prototype 3/Assets/Scripts/MoveLeft.cs
@@ -6,11 +6,25 @@ public class MoveLeft : MonoBehaviour
 { 
 
     private float speed = 30f;
+    private PlayerController playerControllerScript;
+    private float leftBound = -15;
 
-
+    void Start()
+    {
+        playerControllerScript = 
+        GameObject.Find("Player").GetComponent<PlayerController>();
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if(transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- CreateWithCode/Prototype 3/Assets/Scripts/SpawnManager.cs
+++ CreateWithCode/Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -8,15 +8,21 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2f;
     private float repeatRate = 2f;
+    private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript =
+        GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     void SpawnObstacle()
     {
-        Instantiate(obsticalPrefab, spawnPos, obsticalPrefab.transform.rotation);
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obsticalPrefab, spawnPos, obsticalPrefab.transform.rotation);
+        }
     }
 }
